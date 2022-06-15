cb80ea152308644f2fca734064c0cbed7fbc9748
DeanDodds <deandodds88@gmail.com>
2022-06-15 15:25:32 +0000

    added checkout template views forms to checkout app

--- north_yeast_brewing/settings.py
+++ north_yeast_brewing/settings.py
@@ -50,6 +50,10 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+
+    # Other
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -63,6 +67,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'north_yeast_brewing.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,8 +84,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # Reqired by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.context.cart_contents',
             ],
+             'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
