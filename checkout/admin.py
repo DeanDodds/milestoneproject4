919aef3d9cbffa6a3278d65325fa6c54c814d282
DeanDodds <deandodds88@gmail.com>
2022-07-16 11:59:03 +0000

    added profile app functionality

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
+from django.contrib import admin
+from .models import Order, OrderLineItem
+
+
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
@@ -12,15 +16,15 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total', 'original_cart',
+                       'grand_total', 'original_bag',
                        'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
               'order_total', 'grand_total', 'original_cart',
-                       'stripe_pid')
+              'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
@@ -28,5 +32,3 @@ class OrderAdmin(admin.ModelAdmin):
 
     ordering = ('-date',)
 
-
-admin.site.register(Order, OrderAdmin)
