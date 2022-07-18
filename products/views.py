8f3d0223a5c7b0a1d813e32454b1d34ab7fbe22a
DeanDodds <deandodds88@gmail.com>
2022-07-18 11:37:11 +0000

    added product delete view and buttons to product pages

--- products/views.py
+++ products/views.py
@@ -70,6 +70,7 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
+
 def add_product(request):
     """ Add a product to the store """
     if request.method == 'POST':
@@ -113,3 +114,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
