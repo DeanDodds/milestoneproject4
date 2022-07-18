191b892b9dc18573dcccb3fc5790b763bd352eec
DeanDodds <deandodds88@gmail.com>
2022-07-18 19:04:14 +0000

    fix: amended allowed host amended for heroku app

--- north_yeast_brewing/settings.py
+++ north_yeast_brewing/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'True'
 
-ALLOWED_HOSTS = ['north-yeast-brewing.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['north-yeast-brewing.herokuapp.com', 'localhost']
 
 
 # Application definition
