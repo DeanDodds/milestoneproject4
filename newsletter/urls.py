d43f2b34ff6d8af1209350f91309aceac1675244
DeanDodds <deandodds88@gmail.com>
2022-07-22 19:01:09 +0000

    added views to edit and delete newletter subscribers

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -7,5 +7,10 @@ from . import views
 
 urlpatterns = [
     path('', views.newsletter, name='newsletter'),
-    path('newsletter_signup/', views.newsletter, name='newsletter_signup')
+    path('newsletter_success/', views.newsletter, name='newsletter_success'),
+    path('send_newsletter/', views.send_newsletter, name='send_newsletter'),
+    path('view_email_list/', views.view_email_list, name='email_list'),
+    path('edit_mail<int:email_id>/', views.edit_mail, name='edit_mail'),
+    path('delete_email<int:email_id>/', views.delete_mail, name='delete_mail'),
+
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.db import models
 from django.conf import settings
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
+from django.contrib import messages
 
 from .models import NewletterSubscribers
+from .forms import NewletterSubscriberForm
 
 
 
@@ -12,19 +15,19 @@ from .models import NewletterSubscribers
 
 def newsletter(request):
     """ Add user newsletter signups to the database """
+
     if request.method == 'POST':
+        print('emailing all')
         # get email address from form
         email = request.POST['newsletter']
         #if user already has already subscripbed 
         if NewletterSubscribers.objects.filter(email = email).exists():
             message = 'Already subscribed'
-            print(message)
         # if new email
         else:
             # saves form to database
             NewletterSubscribers.objects.create(email=email)
-            print('added')
-            #send email confirmation to user
+            # send email confirmation to user
             send_mail(
                 'North Yeast Brewing',
                 'Hey, You have subscriped to the North Yeast Brewing newletter. We will send you all the lastest events and offers',
@@ -39,4 +42,80 @@ def newsletter(request):
         }
 
     return render(request, 'newsletter/newsletter_success.html', context)
+
+
+def send_newsletter(request):
+    """" send a newsletter out to all subscribers """
+
+    if request.method == 'POST':
+        # Get data from form 
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        # Get emal list from database  
+        email_list = []
+        for subscriber in NewletterSubscribers.objects.all():
+            email_list.append(subscriber.email)
+
+        # send email confirmation to user
+        send_mail(
+            subject,
+            message,
+            'northyeastbrewing.com',
+            [email_list],
+            fail_silently=False,
+        )
+    
+    return render(request, 'newsletter/send_newsletter.html')
+
+
+def view_email_list(request):
+    """ Allow admin to view email list """
+    email_list = NewletterSubscribers.objects.all()
+    
+    context = {
+        'email_list': email_list,
+    }
+
+    return render(request, 'newsletter/view_email_list.html', context)
+
+
+
+def delete_mail(request, email_id):
+    """ Allow admin to delete email subscriber from list """
+    # find email in database 
+    email = get_object_or_404(NewletterSubscribers, pk=email_id)
+    # delet it from databes 
+    email.delete()
+    # get new mailing list from database 
+    email_list = NewletterSubscribers.objects.all()
+
+    context = {
+        'email_list': email_list,
+    }
+    return render(request, 'newsletter/view_email_list.html', context)
+
+
+def edit_mail(request, email_id):
+    """ Allow admin to edit and delete from email list """
+    
+    if request.method == 'POST':
+        updated_email = request.POST['updated-email']
+        email = get_object_or_404(NewletterSubscribers, pk=email_id)
+        email.email = updated_email
+        email.save()
+
+    email_list = NewletterSubscribers.objects.all()
+    context = {
+        'email_list': email_list,
+    }
+
+    return render(request, 'newsletter/view_email_list.html', context)
+
+
+
+
+
+
+
     
