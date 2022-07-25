69742451d7a34c166d0f238dbdca9794fd74c6fa
DeanDodds <deandodds88@gmail.com>
2022-07-25 11:07:31 +0000

    added ourstory.html view and url

--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def story(request):
+    """ A view to return the our story page """
+
+    return render(request, 'home/our_story.html')
