d998923660f4bfd789967eeba34210f95591e3d4
DeanDodds <deandodds88@gmail.com>
2022-07-25 21:04:02 +0000

    style contacts page styles improved

--- events/models.py
+++ events/models.py
@@ -1,11 +1,14 @@
-from django.db import models
 import datetime
+from django.db import models
+
 
 # Create your models here.
+
+
 class Events(models.Model):
     class Meta:
         ordering = ('event_date',)
-        
+
     event_name = models.CharField(max_length=255)
     event_description = models.TextField()
     event_date = models.DateField(("Date"), default=datetime.date.today)
@@ -14,8 +17,3 @@ class Events(models.Model):
     event_location = models.CharField(max_length=255)
     event_ticket_link = models.URLField(max_length=200)
     event_ticket_host = models.CharField(max_length=255)
-    
-
-
-    def __str__(self):
-        return self.event_name
